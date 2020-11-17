39a82576cc038aa9c58d5d78000a3261c62f38a9
Degoke <69061153+Degoke@users.noreply.github.com>
2020-11-17 10:27:28 +0100

    basic frame

---
+++ index.js
@@ -0,0 +1,95 @@
+const dashboardData = [
+    {
+        media: 'facebook',
+        handle: '@nathanf',
+        number: '1987',
+        details: 'Followers',
+        day: '99 Today',
+        status: 'positive'
+
+    },
+    {
+        media: 'twitter',
+        handle: '@nathanf',
+        number: '1044',
+        details: 'Followers',
+        day: '99 Today',
+        status: 'positive'
+    },
+    {
+        media: 'instagram',
+        handle: '@realnathanf',
+        number: '11k',
+        details: 'Followers',
+        day: '1099 Today',
+        status: 'positive'
+    },
+    {
+        media: 'youtube',
+        handle: 'Nathan F.',
+        number: ' 8239',
+        details: 'Subscribers',
+        day: '144 Today',
+        status: 'negative'
+    }
+]
+
+const overviewData = [
+    {
+        media: 'facebook',
+        detials: 'Page Views',
+        number: '87',
+        percentage: '3%',
+        status: 'positive'
+    },
+    {
+        media: 'facebook',
+        detials: 'Likes',
+        number: '52',
+        percentage: '2%',
+        status: 'negative'
+    },
+    {
+        media: 'twitter',
+        detials: 'Likes',
+        number: '5462',
+        percentage: '2257%',
+        status: 'positive'
+    },
+    {
+        media: 'twitter',
+        detials: 'Profile Views',
+        number: '52k',
+        percentage: '1375%',
+        status: 'positive'
+    },
+    {
+        media: 'instagram',
+        detials: 'Retweets',
+        number: '117',
+        percentage: '303%',
+        status: 'positive'
+    },
+    {
+        media: 'instagram',
+        detials: 'Likes',
+        number: '507',
+        percentage: '553%',
+        status: 'positive'
+    },
+    {
+        media: 'youtube',
+        detials: 'Likes',
+        number: '107',
+        percentage: '19%',
+        status: 'negative'
+    },
+    {
+        media: 'youtube',
+        detials: 'Total Views',
+        number: '1407',
+        percentage: '12%',
+        status: 'negative'
+    },
+
+]
